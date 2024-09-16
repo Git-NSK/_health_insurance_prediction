5c1934c24744e7f46a2837c6b72bb90625040636
Sathish Kumar <nsk02git@gmail.com>
2024-09-16 21:51:03 +0530

    Updated file path in prediction_helper using backslash

--- prediction_helper.py
+++ prediction_helper.py
@@ -2,10 +2,10 @@
 import pandas as pd
 import joblib
 
-model_young = joblib.load("artifacts\model_young.joblib")
-model_rest = joblib.load("artifacts\model_older.joblib")
-scaler_young = joblib.load("artifacts\scaler_young.joblib")
-scaler_rest = joblib.load("artifacts\scaler_older.joblib")
+model_young = joblib.load("artifacts/model_young.joblib")
+model_rest = joblib.load("artifacts/model_older.joblib")
+scaler_young = joblib.load("artifacts/scaler_young.joblib")
+scaler_rest = joblib.load("artifacts/scaler_older.joblib")
 
 def calculate_normalized_risk(medical_history):
     risk_scores = {
